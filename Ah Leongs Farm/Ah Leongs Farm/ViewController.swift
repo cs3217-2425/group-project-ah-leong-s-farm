95ea2be003067876860a041e6f3f9488027d5383
Jerry Leong <leongweizhi03@gmail.com>
2025-03-13 15:50:40 +0800

    chore: create skeleton

---
+++ Ah Leongs Farm/Ah Leongs Farm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ah Leongs Farm
+//
+//  Created by Jerry Leong on 13/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ah Leongs Farm/Ah Leongs FarmUITests/Ah_Leongs_FarmUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ah_Leongs_FarmUITestsLaunchTests.swift
+//  Ah Leongs FarmUITests
+//
+//  Created by Jerry Leong on 13/3/25.
+//
+
+import XCTest
+
+final class Ah_Leongs_FarmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
