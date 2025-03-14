0743cb3353b16c95de299a44289f2fc2f1256087
Jerry Leong <leongweizhi03@gmail.com>
2025-03-14 20:08:55 +0800

    feat: add game world

---
+++ Ah Leongs Farm/GameWorld/GameWorld.swift
@@ -0,0 +1,38 @@
+//
+//  GameWorld.swift
+//  Ah Leongs Farm
+//
+//  Created by Jerry Leong on 14/3/25.
+//
+
+import GameplayKit
+
+class GameWorld {
+    typealias Entity = GKEntity
+    typealias System = GKComponentSystem<GKComponent>
+
+    private var entities: Set<Entity> = []
+    private var systems: Set<System> = []
+
+    func addEntity(_ entity: Entity) {
+        entities.insert(entity)
+        for system in systems {
+            system.addComponent(foundIn: entity)
+        }
+    }
+
+    func removeEntity(_ entity: Entity) {
+        entities.remove(entity)
+        for system in systems {
+            system.removeComponent(foundIn: entity)
+        }
+    }
+
+    func addSystem(_ system: System) {
+        systems.insert(system)
+    }
+
+    func removeSystem(_ system: System) {
+        systems.insert(system)
+    }
+}
