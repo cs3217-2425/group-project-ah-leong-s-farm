69712fe29f208badf9c48e328ba1df965df451b0
Lester Ong <lesterong776@gmail.com>
2025-03-18 00:17:42 +0800

    Add item in InventorySystem

--- Ah Leongs Farm/Inventory/Systems/InventorySystem.swift
+++ Ah Leongs Farm/Inventory/Systems/InventorySystem.swift
@@ -9,19 +9,41 @@ import Foundation
 import GameplayKit
 
 class InventorySystem: GKComponentSystem<ItemComponent> {
-    private var items: [GKEntity]
+    private var items: Set<GKEntity>
 
     override init() {
         items = []
         super.init()
     }
 
-    /// Adds an item to the inventory if and only if it is an item component.
+    /// Adds a GKEntity to the inventory if and only if it is an item component.
     /// - Returns: True if the item is added successfully, false otherwise.
     @discardableResult
     func addItem(_ itemToAdd: GKEntity) -> Bool {
-        guard let item = itemToAdd.component(ofType: ItemComponent.self) else {
+        guard let itemComponent = itemToAdd.component(ofType: ItemComponent.self) else {
             return false
+        }
+
+        guard itemComponent.quantity > 0 else {
+            return false
+        }
+
+        if !itemComponent.stackable {
+            items.insert(itemToAdd)
+            return true
+        }
+
+        for existingItem in self.items {
+            guard let existingItemComponent = existingItem.component(ofType: ItemComponent.self) else {
+                continue
+            }
+
+            guard existingItemComponent.itemType == itemComponent.itemType else {
+                continue
+            }
+
+            existingItemComponent.add(itemComponent.quantity)
+            return true
         }
 
         return false
