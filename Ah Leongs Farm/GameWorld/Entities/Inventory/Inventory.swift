aa252e4017d492fbbba5301a946773dafdb0c801
Lester Ong <lesterong776@gmail.com>
2025-03-20 01:41:45 +0800

    Add inventory entity and component

---
+++ Ah Leongs Farm/GameWorld/Entities/Inventory/Inventory.swift
@@ -0,0 +1,25 @@
+//
+//  Inventory.swift
+//  Ah Leongs Farm
+//
+//  Created by Lester Ong on 20/3/25.
+//
+
+import Foundation
+import GameplayKit
+
+class Inventory: GKEntity {
+    override init() {
+        super.init()
+        setUpComponents()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) not implemented")
+    }
+
+    private func setUpComponents() {
+        let inventoryComponent = InventoryComponent()
+        addComponent(inventoryComponent)
+    }
+}
