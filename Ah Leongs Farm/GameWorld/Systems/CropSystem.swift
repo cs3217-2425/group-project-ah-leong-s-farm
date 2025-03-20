94e1ed6812b2391a372204468a2dfe7a595bbb34
Jerry Leong <leongweizhi03@gmail.com>
2025-03-20 17:23:06 +0800

    Clean up PR

--- Ah Leongs Farm/GameWorld/Systems/CropSystem.swift
+++ Ah Leongs Farm/GameWorld/Systems/CropSystem.swift
@@ -8,9 +8,12 @@ class CropSystem: GKComponentSystem<CropComponent> {
 
     /// Create a new entity for a crop
     func plantCrop(ofType cropType: CropType, grid: GridComponent, row: Int, column: Int) -> CropComponent? {
-        guard let entity = grid.getObject(row: row, column: column),
-              let position = entity.component(ofType: PositionComponent.self),
-              let soil = entity.component(ofType: SoilComponent.self) else {
+        guard let entity = grid.getObject(row: row, column: column) else {
+            return nil
+        }
+
+        // Disallow planting on entity with an existing crop
+        if entity.component(ofType: CropComponent.self) != nil {
             return nil
         }
 
@@ -22,18 +25,27 @@ class CropSystem: GKComponentSystem<CropComponent> {
             plantedTurn: 1
         )
 
+        // Add crop component to entity
         entity.addComponent(crop)
+
+        // Add crop component to system
+        addComponent(crop)
+
         return crop
     }
 
-    /// Remove crop entity and add harvested crop entityx
+    /// Remove crop entity and add harvested crop entity
     func harvestCrop(grid: GridComponent, row: Int, column: Int) -> CropComponent? {
         guard let entity = grid.getObject(row: row, column: column),
               let crop = entity.component(ofType: CropComponent.self) else {
             return nil
         }
 
+        // Remove crop component from entity
         entity.removeComponent(ofType: CropComponent.self)
+
+        // Remove crop component from system
+        removeComponent(crop)
 
         return crop
     }
--- Ah Leongs Farm/GameWorld/Systems/ISystem.swift
+++ Ah Leongs Farm/GameWorld/Systems/ISystem.swift
@@ -1,8 +1,6 @@
 import GameplayKit
 
 protocol ISystem: AnyObject {
-    var componentClass: AnyClass { get }
-
     func addComponent(foundIn entity: GKEntity)
 
     func removeComponent(foundIn entity: GKEntity)
