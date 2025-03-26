e75a3c319206a37057bcf0a9711bf251e8f97077
Carmen <carmen.ang12@gmail.com>
2025-03-27 03:42:31 +0800

    remove EventType

--- Ah Leongs Farm/Events/DataStructures/EventData.swift
+++ Ah Leongs Farm/Events/DataStructures/EventData.swift
@@ -6,28 +6,23 @@
 //
 
 protocol EventData {
-    var eventType: EventType { get }
 }
 
 struct EndTurnEventData: EventData {
-    var eventType: EventType = .endTurn
     var endTurnCount: Int = 1
 }
 
 struct HarvestCropEventData: EventData {
-    var eventType: EventType = .harvestCrop
     var type: CropType
     var quantity: Int
 }
 
 struct SellCropEventData: EventData {
-    var eventType: EventType = .sellCrop
     var type: CropType
     var quantity: Int
 }
 
 struct RewardGrantEventData: EventData {
-    var eventType: EventType = .rewardGrant
     var xpGrantAmount: Int = 0
     var currencyGrants: [CurrencyType: Double] = [:]
     var itemGrants: [ItemType: Int] = [:]
