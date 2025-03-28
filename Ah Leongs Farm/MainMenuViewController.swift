3a85a7f0b1e1392c4e0544f5f84464f1ea272906
Lester Ong <lesterong776@gmail.com>
2025-03-28 17:55:33 +0800

    Create buttons programatically

--- Ah Leongs Farm/MainMenuViewController.swift
+++ Ah Leongs Farm/MainMenuViewController.swift
@@ -12,17 +12,41 @@ class MainMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let titleLabel = UILabel()
+        titleLabel.text = "Ah Leong's Farm"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let startButton = UIButton(type: .system)
+        startButton.setTitle("Start Game", for: .normal)
+        startButton.backgroundColor = .systemBlue
+        startButton.setTitleColor(.white, for: .normal)
+        startButton.layer.cornerRadius = 10
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(titleLabel)
+        view.addSubview(startButton)
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: startButton.topAnchor, constant: -20),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 20),
+            startButton.widthAnchor.constraint(equalToConstant: 200),
+            startButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
     }
 
-    @IBAction private func startPressed(_ sender: UIButton) {
+    @objc func startButtonTapped() {
         performSegue(withIdentifier: "gameView", sender: self)
     }
 
     // Override prepare method to set full screen presentation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "gameView" {
-            // Set the destination view controller to full screen
             segue.destination.modalPresentationStyle = .fullScreen
         }
     }
