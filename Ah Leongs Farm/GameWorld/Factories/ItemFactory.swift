ce6d6fd3f24f658962f1f5149218eb6cf92f5de7
yuchen <88711624+dawg420@users.noreply.github.com>
2025-04-04 15:59:55 +0800

    Revert "Refactored ItemFactory"
    This reverts commit 8141601765ecc165a492933a50fa733f5338a9c6.

--- Ah Leongs Farm/GameWorld/Factories/ItemFactory.swift
+++ Ah Leongs Farm/GameWorld/Factories/ItemFactory.swift
@@ -11,23 +11,20 @@ import GameplayKit
 class ItemFactory {
     static let itemToInitialisers: [ItemType: () -> GKEntity?] = [
         .bokChoySeed: {
-            createSeed(for: .bokChoy)
-                .setupSellComponent()
-        },
+            createSeed(for: .bokChoy).flatMap { setupComponents($0, type: .bokChoySeed) }
+            },
         .appleSeed: {
-            createSeed(for: .apple)
-                .setupSellComponent()
-        },
+            createSeed(for: .apple).flatMap { setupComponents($0, type: .appleSeed) }
+            },
         .potatoSeed: {
-            createSeed(for: .potato)
-                .setupSellComponent()
-        },
+            createSeed(for: .potato).flatMap { setupComponents($0, type: .potatoSeed) }
+             },
         .fertiliser: {
-            Fertiliser().setupSellComponent()
-        },
+            setupComponents(Fertiliser(), type: .fertiliser)
+            },
         .premiumFertiliser: {
-            PremiumFertiliser().setupSellComponent()
-        }
+            setupComponents(PremiumFertiliser(), type: .premiumFertiliser)
+            }
     ]
 
     private static let cropToSeedInitialisers: [CropType: () -> GKEntity] = [
@@ -36,10 +33,22 @@ class ItemFactory {
         .potato: { Potato.createSeed() }
     ]
 
-    private static func createSeed(for crop: CropType) -> GKEntity {
-        guard let seedInitialiser = cropToSeedInitialisers[crop] else {
-            fatalError("Seed initialiser for \(crop) not defined!")
+    private static func createSeed(for crop: CropType) -> GKEntity? {
+        cropToSeedInitialisers[crop]?()
+    }
+
+    private static func addItemComponent(_ entity: GKEntity, type: ItemType) -> GKEntity {
+        entity.addComponent(ItemComponent(itemType: type))
+        return entity
+    }
+
+    private static func setupComponents(_ entity: GKEntity, type: ItemType) -> GKEntity {
+        entity.addComponent(ItemComponent(itemType: type))
+
+        // Add SellComponent if the market can sell that item
+        if MarketInformation.sellableItems.contains(type) {
+            entity.addComponent(SellComponent(itemType: type))
         }
-        return seedInitialiser()
+        return entity
     }
 }
