d4f068642768bdd4cc9203f9b7c9694f42b0777d
Jerry Leong <leongweizhi03@gmail.com>
2025-04-05 20:24:22 +0800

    Add energy deduction and xp gain

--- Ah Leongs Farm/Events/CropEvents/HarvestCropEvent.swift
+++ Ah Leongs Farm/Events/CropEvents/HarvestCropEvent.swift
@@ -9,21 +9,30 @@ struct HarvestCropEvent: GameEvent {
     let row: Int
     let column: Int
 
+    private let ENERGY_USAGE = 1
+    private let XP_AMOUNT: Float = 10.0
+
     func execute(in context: any EventContext, queueable: any EventQueueable) -> (any EventData)? {
-        guard let cropSystem = context.getSystem(ofType: CropSystem.self) else {
+        guard let cropSystem = context.getSystem(ofType: CropSystem.self),
+              let energySystem = context.getSystem(ofType: EnergySystem.self),
+              let levelSystem = context.getSystem(ofType: LevelSystem.self) else {
             return nil
         }
 
-        guard let harvestedCrop = cropSystem.harvestCrop(row: row, column: column) else {
+        guard energySystem.getCurrentEnergy() >= ENERGY_USAGE else {
             return nil
         }
 
-        guard let cropComponent = harvestedCrop.component(ofType: CropComponent.self) else {
+        guard let harvestedCrop = cropSystem.harvestCrop(row: row, column: column),
+              let cropComponent = harvestedCrop.component(ofType: CropComponent.self) else {
             return nil
         }
 
         // Set harvested quantity to 1 for now
         let harvestedQuantity = 1
+
+        energySystem.useEnergy(amount: ENERGY_USAGE)
+        levelSystem.addXP(XP_AMOUNT)
 
         return HarvestCropEventData(type: cropComponent.cropType, quantity: harvestedQuantity)
     }
