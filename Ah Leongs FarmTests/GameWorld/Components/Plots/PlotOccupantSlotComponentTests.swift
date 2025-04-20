6d09e11d19cc4b5c2ab8d9b97bf0f7d50d5918ed
Lester Ong <lesterong776@gmail.com>
2025-04-20 17:45:17 +0800

    Add tests

---
+++ Ah Leongs FarmTests/GameWorld/Components/Plots/PlotOccupantSlotComponentTests.swift
@@ -0,0 +1,48 @@
+//
+//  PlotOccupantSlotComponentTests.swift
+//  Ah Leongs FarmTests
+//
+//  Created by Lester Ong on 25/3/25.
+//
+
+import XCTest
+@testable import Ah_Leongs_Farm
+
+class PlotOccupantSlotComponentTests: XCTestCase {
+    class MockPlotOccupant: EntityAdapter, PlotOccupant {}
+    
+    func testInitWithNilPlotOccupant() {
+        let component = PlotOccupantSlotComponent()
+        
+        XCTAssertNotNil(component)
+        XCTAssertNil(component.plotOccupant)
+    }
+    
+    func testInitWithPlotOccupant() {
+        let mockOccupant = MockPlotOccupant()
+        let component = PlotOccupantSlotComponent(plotOccupant: mockOccupant)
+        
+        XCTAssertNotNil(component)
+        XCTAssertNotNil(component.plotOccupant)
+        XCTAssertTrue(component.plotOccupant === mockOccupant)
+    }
+    
+    func testSetPlotOccupant() {
+        let component = PlotOccupantSlotComponent()
+        let mockOccupant = MockPlotOccupant()
+        
+        component.plotOccupant = mockOccupant
+        
+        XCTAssertNotNil(component.plotOccupant)
+        XCTAssertTrue(component.plotOccupant === mockOccupant)
+    }
+    
+    func testSetPlotOccupantToNil() {
+        let mockOccupant = MockPlotOccupant()
+        let component = PlotOccupantSlotComponent(plotOccupant: mockOccupant)
+        
+        component.plotOccupant = nil
+        
+        XCTAssertNil(component.plotOccupant)
+    }
+} 
